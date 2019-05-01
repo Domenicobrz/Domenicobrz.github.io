1c4bcfbd39afae4533d67e1c921b6fb937de00c9
Domenicobrz <domenicobrz@gmail.com>
2019-05-01 17:59:46 +0200

    jwd finished

--- temp/jwdebug/php/get-viewer-data.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// $file = file_get_contents("https://drapr.com/viewer/json-api/single_item_viewer_api.php?" . $_SERVER['QUERY_STRING']);
-$file = file_get_contents("../assets/JSON/viewer-data-colors.json");
-echo $file;
-
-?>
