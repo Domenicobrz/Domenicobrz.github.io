090ce938394d630d40541745c22464c36f1d4fe4
Domenicobrz <domenicobrz@gmail.com>
2020-02-24 16:28:12 +0100

    updated website

--- webgl/webpack source/src/libs/posts.js
+++ webgl/webpack source/src/libs/posts.js
@@ -1,6 +1,7 @@
 var posts = [
     { imgPath: "images/projects/caustics.jpg",  link: "http://domenicobrz.github.io/webgl/projects/dispersion-caustics/", description: "", tags: ["webgl"] },
     { imgPath: "images/projects/lumen2d.jpg",  link: "https://github.com/Domenicobrz/Lumen-2D", description: "", tags: ["geometry", "misc"] },
+    { imgPath: "images/projects/rtptcornellbox.jpg",  link: "http://domenicobrz.github.io/webgl/projects/RTPTCornellBox/", description: "", tags: ["threejs", "webgl"] },
     { imgPath: "images/projects/volumetricart.png",  link: "http://domenicobrz.github.io/webgl/projects/Volumetric art/", description: "", tags: ["webgl"] },
     { imgPath: "images/projects/frontend2.jpg",  link: "http://domenicobrz.github.io/webgl/projects/frontend 2/", description: "", tags: ["threejs", "misc"] },
     { imgPath: "images/projects/sidescroller.jpg",  link: "http://domenicobrz.github.io/webgl/projects/Sidescroller anim/", description: "", tags: ["webgl"] },
