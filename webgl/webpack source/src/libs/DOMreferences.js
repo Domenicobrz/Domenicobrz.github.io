f46d7cbeede310061cea7460d5afc7be03e06df9
Domenicobrz <domenicobrz@gmail.com>
2022-10-22 14:54:03 +0200

    added new painting

--- webgl/webpack source/src/libs/DOMreferences.js
+++ webgl/webpack source/src/libs/DOMreferences.js
@@ -6,7 +6,11 @@ var dom = {
     
     TagSelectors: document.getElementsByClassName("TagSelector"),
 
-    SketchesContainer: document.querySelector(".sketches_container"),
+    SketchesContainer: document.querySelector(".graphics_sketches"),
+    SketchesPaginator: document.querySelector(".graphics_sketches_paginator"),
+
+    PaintingsContainer: document.querySelector(".paintings_sketches"),
+    PaintingsPaginator: document.querySelector(".paintings_sketches_paginator"),
 
     main: document.querySelector("body > main"),
 };
--- webgl/webpack source/src/libs/index.js
+++ webgl/webpack source/src/libs/index.js
@@ -1,8 +1,12 @@
 import style from "./../css/main.scss";
 import { initPaginator } from "./paginator";
-import { initSketches } from "./sketchesLoader";
+import { SketchGallery } from "./sketchesLoader";
 import LegendaryCursor from "legendary-cursor";
+import { sketches } from "./sketches";
+import { paintings } from "./paintings";
+import { dom } from "./DOMreferences";
 
 initPaginator();
-initSketches();
+new SketchGallery(sketches, dom.SketchesContainer, dom.SketchesPaginator, "#graphics_sketches_header");
+new SketchGallery(paintings, dom.PaintingsContainer, dom.PaintingsPaginator, "#paintings_sketches_header");
 LegendaryCursor.init();
